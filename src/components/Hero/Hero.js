66f11217c633ae1c44c0688080e8bfd1f033c6f6
Aamir Nathani <amirnathani9@gmail.com>
2022-04-10 02:55:40 +0530

    feat: added routing links

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Hero() {
   return (
       <section className="main-image-container section-center flex items-center justify-center flex-col border-radius-2 my-8">
         <h1 className="mb-2">Watches World</h1>
         <p className="font-size-6 mb-4">Best Watches Are Here</p>
-        <a href="./pages/product.html">
+        <Link to="/product">
           <button className="btn primary-outline-btn font-size-5 border-radius-2 px-8 py-4">
             Shop Now
           </button>
-        </a>
+        </Link>
       </section>
   );
 }
