eef17e73685510c1e0d7d7c5086821fd05c76a50
Aamir Nathani <amirnathani9@gmail.com>
2022-04-11 02:41:02 +0530

    feat: added api call for categories

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,24 +1,9 @@
-import { categories } from "../../backend/db/categories";
-
-export function Brands() {
+export function Brands({ _id, categoryName, image }) {
   return (
     <>
-      <section className="top-brands-container section-center flex flex-col justify-ceneter items-center my-8">
-        <h1 className="section-heading font-bold border-radius-1 my-8 p-2">
-          Top Brands
-        </h1>
-        <main className="flex flex-wrap justify-center">
-          {categories.map((item) => (
-            <div className="brand-items m-8">
-              <img
-                src={item.image}
-                alt={item.categoryName}
-                className="responsive-img"
-              />
-            </div>
-          ))}
-        </main>
-      </section>
+      <div className="brand-items m-8">
+        <img src={image} alt={categoryName} className="responsive-img" />
+      </div>
     </>
   );
 }
--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -1,13 +1,42 @@
 import "./homepage.css"
+import axios from "axios";
 import { Brands, Hero } from "../../components";
 import { useDocumentTitle } from "../../hooks/useDocumentTitle";
+import { useEffect, useState } from "react";
+
 
 export function Homepage() {
+
+  const [productCategories, setProductCategories] = useState([])
+
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const categoriesData = await axios.get("/api/categories");
+        setProductCategories(categoriesData.data.categories)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  })
+
   useDocumentTitle("Watches World")
   return (
     <>
       <Hero />
-      <Brands />
+      <section className="top-brands-container section-center flex flex-col justify-ceneter items-center my-8">
+        <h1 className="section-heading font-bold border-radius-1 my-8 p-2">
+          Top Brands
+        </h1>
+        <main className="flex flex-wrap justify-center">
+          {productCategories.map(({ _id, categoryName, image }) => (
+            <Brands key={_id} categoryName={categoryName} image={image} />
+          ))}
+        </main>
+      </section>
+
+
     </>
   );
 }
