5568232181b9a394b0dff9a8ec5f9b7f175f5ad0
Aamir Nathani <amirnathani9@gmail.com>
2022-04-14 17:25:25 +0530

    fix: route name

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ export function Hero() {
       <section className="main-image-container section-center flex items-center justify-center flex-col border-radius-2 my-8">
         <h1 className="mb-2">Watches World</h1>
         <p className="font-size-6 mb-4">Best Watches Are Here</p>
-        <Link to="/product">
+        <Link to="/products">
           <button className="btn primary-outline-btn font-size-5 border-radius-2 px-8 py-4">
             Shop Now
           </button>
