cf360a2b4eec78058d93b0575983216bea38fc07
Aamir Nathani <amirnathani9@gmail.com>
2022-04-18 03:23:37 +0530

    refactor: format document

--- src/utilities/wishlist/removeFromWishlist.js
+++ src/utilities/wishlist/removeFromWishlist.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { encodedToken } from "../token";
 
-export const removeFromWishlistHandler = async (productId,setWishlistItems) => {
-    try {
-      const response = await axios.delete(`/api/user/wishlist/${productId}`, {
-        headers: { authorization: encodedToken },
-      });
-      setWishlistItems(response.data.wishlist);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const removeFromWishlistHandler = async (
+  productId,
+  setWishlistItems
+) => {
+  try {
+    const response = await axios.delete(`/api/user/wishlist/${productId}`, {
+      headers: { authorization: encodedToken },
+    });
+    setWishlistItems(response.data.wishlist);
+  } catch (error) {
+    console.log(error);
+  }
+};
