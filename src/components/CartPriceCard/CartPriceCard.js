73df6c0a6d5675268d748f57362472878ab936b1
Aamir Nathani <amirnathani9@gmail.com>
2022-04-17 20:48:04 +0530

    feat: added price of carts in price cart

--- src/components/CartPriceCard/CartPriceCard.js
+++ src/components/CartPriceCard/CartPriceCard.js
@@ -1,26 +1,35 @@
-export const CartPriceCard = () => {
+import {useCart} from "../../contexts/cart-context"
+
+export const CartPriceCard = ({}) => {
+  const {cartItems} = useCart()
+
+  const deliveryCharges = 499
+  const amount = cartItems.reduce((acc,curr)=>acc + Number(curr.originalPrice)* curr.qty,0).toFixed(2)
+  const discount = cartItems.reduce((acc,curr)=>acc + Number(curr.originalPrice*10/100) * curr.qty,0).toFixed(2)
+  const totalAmount =( amount - discount + deliveryCharges ).toFixed(2)
+
   return (
     <section className="cart-price-section flex flex-col items-center">
       <div className="card-price-container flex flex-col border-radius-2 m-8 p-8">
         <h2 className="font-bold my-3">Price Details</h2>
 
         <div className="flex justify-between font-bold my-3">
-          <span>Price</span>
-          <span>₹9,999.00</span>
+          <span>Price (Items: {cartItems.length})</span>
+          <span>₹{amount}</span>
         </div>
         <div className="flex justify-between my-3">
           <span>Discount</span>
-          <span>₹4,999.00</span>
+          <span>₹{discount}</span>
         </div>
         <div className="flex justify-between my-3">
           <span>Delivery Charges</span>
-          <span>₹999.00</span>
+          <span>₹{deliveryCharges}</span>
         </div>
         <div className="flex justify-between font-bold my-3">
           <span>Total Amount</span>
-          <span>₹5,998.00</span>
+          <span>₹{totalAmount}</span>
         </div>
-        <p className="my-3">You will save Rs ₹4,999.00 on this order</p>
+        <p className="my-3">You will save Rs ₹{discount} on this order</p>
         <button className="btn primary-outline-btn font-size-5 border-radius-1">
           Proceed to Checkout
         </button>
