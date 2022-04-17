0f6f1fc33e7949d3264e23c8b85c17596140c282
Aamir Nathani <amirnathani9@gmail.com>
2022-04-17 05:39:43 +0530

    feat: map the cartItems on cartPage

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,6 +1,9 @@
+import { useCart } from "../../contexts";
 import { useDocumentTitle } from "../../hooks/useDocumentTitle";
 import "./Cart.css";
 export function Cart() {
+  const { cartItems } = useCart();
+
   useDocumentTitle("Cart - Watches World");
   return (
     <div class="flex flex-col justify-center items-center my-4">
@@ -9,68 +12,56 @@ export function Cart() {
       </h1>
       <div class="cart-main-container section-center flex">
         <section class="cart-cards-section flex flex-col">
-          <div class="card card-horizontal card-shadow border-radius-1 m-8">
-            <div class="card-horizontal-image">
-              <img
-                src="https://nomark-ui.netlify.app/assests/card-image.jpeg"
-                alt="watches"
-                class="card-img border-radius-1"
-              />
-            </div>
-            <div class="card-content">
-              <div class="card-heading">
-                <h3>Tag Heuer-CR7</h3>
+          {cartItems.map(
+            ({
+              _id,
+              title,
+              model,
+              image,
+              originalPrice,
+              discountedPrice,
+              discount,
+              qty,
+            }) => (
+              <div class="card card-horizontal card-shadow border-radius-1 m-8">
+                <div class="card-horizontal-image">
+                  <img
+                    src={image}
+                    alt="watches"
+                    class="card-img border-radius-1"
+                  />
+                </div>
+                <div class="card-content">
+                  <div class="card-heading">
+                    <h3>{title}</h3>
+                  </div>
+                  <div class="card-product-price">
+                    <span class="product-discount-price">
+                      Rs.{discountedPrice}
+                    </span>
+                    <span class="product-original-price">
+                      Rs.{originalPrice}
+                    </span>
+                  </div>
+                  <div class="flex mb-3 items-center">
+                    <span>Quantity: </span>
+                    <span class="quantity-remove"><i className="fa fa-minus"></i></span>
+                    <span class="card-quantity font-bold">{qty}</span>
+                    <span class="quantity-add">
+                      <i className="fa fa-plus"></i>
+                    </span>
+                  </div>
+                  <button class="card-btn font-size-4 border-radius-1">
+                    <i class="fas fa-shopping-cart card-btn-icon"></i>
+                    Move To wishlist
+                  </button>
+                  <button class="card-secondary-btn font-size-4 border-radius-1">
+                    Remove from Cart
+                  </button>
+                </div>
               </div>
-              <div class="card-product-price">
-                <span class="product-discount-price">₹4,999.00</span>
-                <span class="product-original-price">₹9,999.00</span>
-              </div>
-              <div class="flex mb-3 items-center">
-                <span>Quantity: </span>
-                <span class="quantity-remove"></span>
-                <span class="card-quantity">1</span>
-                <span class="quantity-add"> </span>
-              </div>
-              <button class="card-btn font-size-4 border-radius-1">
-                <i class="fas fa-shopping-cart card-btn-icon"></i>
-                Move To wishlist
-              </button>
-              <button class="card-secondary-btn font-size-4 border-radius-1">
-                Remove from Cart
-              </button>
-            </div>
-          </div>
-          <div class="card card-horizontal card-shadow border-radius-1 m-8">
-            <div class="card-horizontal-image">
-              <img
-                src="https://nomark-ui.netlify.app/assests/card-image.jpeg"
-                alt="watches"
-                class="card-img border-radius-1"
-              />
-            </div>
-            <div class="card-content">
-              <div class="card-heading">
-                <h3>Tag Heuer-CR7</h3>
-              </div>
-              <div class="card-product-price">
-                <span class="product-discount-price">₹4,999.00</span>
-                <span class="product-original-price">₹9,999.00</span>
-              </div>
-              <div class="flex mb-3 items-center">
-                <span>Quantity: </span>
-                <span class="quantity-remove"></span>
-                <span class="card-quantity">1</span>
-                <span class="quantity-add"> </span>
-              </div>
-              <button class="card-btn font-size-4 border-radius-1">
-                <i class="fas fa-shopping-cart card-btn-icon"></i>
-                Move To wishlist
-              </button>
-              <button class="card-secondary-btn font-size-4 border-radius-1">
-                Remove from Cart
-              </button>
-            </div>
-          </div>
+            )
+          )}
         </section>
         <section class="cart-price-section flex flex-col items-center">
           <div class="card-price-container flex flex-col border-radius-2 m-8 p-8">
