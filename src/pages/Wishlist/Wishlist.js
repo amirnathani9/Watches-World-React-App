fbfe8056928305a0b06f6a5d5bae2417375052cf
Aamir Nathani <amirnathani9@gmail.com>
2022-04-18 03:12:51 +0530

    remove: unwanted things

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,15 +1,9 @@
 import "./Wishlist.css";
 import { useDocumentTitle } from "../../hooks/useDocumentTitle";
-import { useWishlist } from "../../contexts";
+
 import { WishlistCard } from "../../components";
-import axios from "axios";
-import { encodedToken } from "../../utilities/token";
 
 export const Wishlist = () => {
-  const { wishlistItems, setWishlistItems } = useWishlist();
-
-  
-
   useDocumentTitle("Wishlist - Watches World");
   return (
     <section className="section-center flex flex-col justify-ceneter items-center my-4">
@@ -17,30 +11,30 @@ export const Wishlist = () => {
         My Wishlist
       </h1>
       <div>
-      {wishlistItems.map(
-        ({
-          _id,
-          title,
-          model,
-          image,
-          originalPrice,
-          discountedPrice,
-          discount,
-          qty,
-        }) => (
-          <WishlistCard
-            key={_id}
-            _id={_id}
-            title={title}
-            model={model}
-            image={image}
-            originalPrice={originalPrice}
-            discountedPrice={discountedPrice}
-            discount={discount}
-            // removeFromWishlistHandler={removeFromWishlistHandler}
-          />
-        )
-      )}
+        {wishlistItems.map(
+          ({
+            _id,
+            title,
+            model,
+            image,
+            originalPrice,
+            discountedPrice,
+            discount,
+            qty,
+          }) => (
+            <WishlistCard
+              key={_id}
+              _id={_id}
+              title={title}
+              model={model}
+              image={image}
+              originalPrice={originalPrice}
+              discountedPrice={discountedPrice}
+              discount={discount}
+              // removeFromWishlistHandler={removeFromWishlistHandler}
+            />
+          )
+        )}
       </div>
     </section>
   );
